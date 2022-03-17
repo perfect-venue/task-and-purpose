a242ca163fafc80032d34cc604192c8660110cdf
Matt Walters <matt@perfectvenue.com>
2022-03-17 15:51:05 -0700

    Appbar cleanup

--- web/src/components/AppBar.js
+++ web/src/components/AppBar.js
@@ -1,6 +1,4 @@
-import MenuIcon from "@mui/icons-material/Menu";
 import AppBar from "@mui/material/AppBar";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -15,13 +13,10 @@ const TaskAppBar = () => (
         color="inherit"
         aria-label="menu"
         sx={{ mr: 2 }}
-      >
-        <MenuIcon />
-      </IconButton>
+      ></IconButton>
       <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
         Task and Purpose
       </Typography>
-      <Button color="inherit">Login</Button>
     </Toolbar>
   </AppBar>
 );
