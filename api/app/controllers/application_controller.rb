de5a87bee2bc5426d2851d22efbd548af695c848
Matt Walters <matt@perfectvenue.com>
2022-04-07 12:26:40 -0700

    Alt CORS config

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  before_action :cors_set_access_control_headers
+
+  def cors_preflight_check
+    return unless request.method == 'OPTIONS'
+    cors_set_access_control_headers
+    render json: {}
+  end
+
+  protected
+
+  def cors_set_access_control_headers
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, ' \
+      'Auth-Token, Email, X-User-Token, X-User-Email, x-xsrf-token'
+    response.headers['Access-Control-Max-Age'] = '1728000'
+    response.headers['Access-Control-Allow-Credentials'] = true
+  end
 end
