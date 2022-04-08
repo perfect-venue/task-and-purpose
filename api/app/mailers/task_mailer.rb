2a03747e126bce89b4133bb2ada57230fe3914fb
Matt Walters <matt@perfectvenue.com>
2022-04-08 14:06:12 -0700

    Allow nil task on Mailer

--- api/app/mailers/task_mailer.rb
+++ api/app/mailers/task_mailer.rb
@@ -1,6 +1,6 @@
 class TaskMailer < ApplicationMailer
   def example_email
-    @task = Task.find(params[:task_id])
+    @task = Task.find_by(id: params[:task_id])
     mail(to: 'exmaple@email.com', subject: 'Welcome to Task and Purpose')
   end
 end
