a2e45a09cd929991b4eaf1204c00c2361a55b6c1
Noah Peden <noah@wespire.com>
2022-04-18 15:36:15 -0400

    chore: user and task owner boilerplate rails

---
+++ api/app/graphql/types/user_type.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :email, String, null: false
+    field :full_name, String, null: false
+
+    def full_name
+      # `object` references the user instance
+      [object.first_name, object.last_name].compact.join(' ')
+    end
+  end
+end
