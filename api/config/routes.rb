0d4e74f1ad5aafbf717d812b4b407f7f4592fd01
Matt Walters <matt@perfectvenue.com>
2022-04-29 11:56:48 -0700

    Adds Graphiql

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
 end
