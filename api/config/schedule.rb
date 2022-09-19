80a2d78ccffbe27f3300d04dfeaade5a4e2d5117
jbotwina <jimbotwina16@gmail.com>
2022-09-19 10:35:07 -0400

    make bundle work

--- api/config/schedule.rb
+++ api/config/schedule.rb
@@ -1,4 +1,6 @@
+set :bundle_command, '<your path to bundle> exec'
 set :output, './log/cron.log'
+
 every 1.days, at: '4:30 am' do
     runner "TaskStatusChangedMailer.secheduleTaskNotifications"
 end
